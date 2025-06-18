7f2f6609ebea128694f7ac12e053a3be07129d6a
itqonibras <itqonnibras@gmail.com>
2025-06-18 20:43:05 +0700

    chore: separate profile mapper

---
+++ temani/src/main/java/com/temani/temani/features/profile/infrastructure/mapper/CaregiverProfileDtoMapper.java
@@ -0,0 +1,11 @@
+package com.temani.temani.features.profile.infrastructure.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.temani.temani.features.profile.domain.model.CaregiverProfile;
+import com.temani.temani.features.profile.presentation.dto.response.CaregiverProfileResponse;
+
+@Mapper(componentModel = "spring")
+public interface CaregiverProfileDtoMapper {
+    CaregiverProfileResponse toDto(CaregiverProfile domain);
+}
---
+++ temani/src/main/java/com/temani/temani/features/profile/infrastructure/mapper/ClientProfileDtoMapper.java
@@ -0,0 +1,11 @@
+package com.temani.temani.features.profile.infrastructure.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.temani.temani.features.profile.domain.model.ClientProfile;
+import com.temani.temani.features.profile.presentation.dto.response.ClientProfileResponse;
+
+@Mapper(componentModel = "spring")
+public interface ClientProfileDtoMapper {
+    ClientProfileResponse toDto(ClientProfile domain);
+}
